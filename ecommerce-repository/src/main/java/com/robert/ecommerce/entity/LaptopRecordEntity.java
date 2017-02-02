c823a06c1fcad7817df570e044c6d15aa0894535
Jakub Salamon <awruk8@gmail.com>
2017-02-02 12:24:51 +0100

    laptop record entity

---
+++ ecommerce-repository/src/main/java/com/robert/ecommerce/entity/LaptopRecordEntity.java
@@ -0,0 +1,15 @@
+package com.robert.ecommerce.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LaptopRecordEntity {
+
+    private double displayRes;
+    private double displaySize;
+    private double hddSize;
+}
