9810124cf8cc012ad6cfe24d9c6e3642ac159f45
Jakub Salamon <awruk8@gmail.com>
2017-02-02 12:41:10 +0100

    getRecords

--- ecommerce-repository/src/main/java/com/robert/ecommerce/repository/LoggerRepository.java
+++ ecommerce-repository/src/main/java/com/robert/ecommerce/repository/LoggerRepository.java
@@ -1,9 +1,14 @@
 package com.robert.ecommerce.repository;
 
+import com.robert.ecommerce.entity.LaptopRecordEntity;
 import com.robert.ecommerce.entity.LoggingEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.stream.Stream;
 
 public interface LoggerRepository extends CrudRepository<LoggingEntity, Long> {
 
-
+    @Query("select n.id, (n.displayResolution.width * n.displayResolution.hight), n.hardDisc.size, l.time  form notebook n join  logger l on l.productId = n.id")
+    Stream<LaptopRecordEntity> getRecords();
 }
