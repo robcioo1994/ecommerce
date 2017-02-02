efcc3a69dc6b8cfa1785f940adc811b5f0599e14
robert <robcioo1994.nak@gmail.com>
2017-02-02 12:51:05 +0100

    query

--- ecommerce-repository/src/main/java/com/robert/ecommerce/repository/LoggerRepository.java
+++ ecommerce-repository/src/main/java/com/robert/ecommerce/repository/LoggerRepository.java
@@ -1,6 +1,6 @@
 package com.robert.ecommerce.repository;
 
-import com.robert.ecommerce.entity.LaptopRecordEntity;
+import com.robert.ecommerce.entity.DisplayTypeEntity;
 import com.robert.ecommerce.entity.LoggingEntity;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -9,6 +9,8 @@ import java.util.stream.Stream;
 
 public interface LoggerRepository extends CrudRepository<LoggingEntity, Long> {
 
-    @Query("select n.id, (n.displayResolution.width * n.displayResolution.hight), n.hardDisc.size, l.time  form notebook n join  logger l on l.productId = n.id")
-    Stream<LaptopRecordEntity> getRecords();
+//    @Query("select n.id, (n.displayResolution.width * n.displayResolution.hight), n.hardDisc.size, l.time  form notebook n join  logger l on l.productId = n.id")
+    @Query("select u from DisplayTypeEntity u")
+    Stream<DisplayTypeEntity> getRecords();
+
 }
--- ecommerce-rest/src/main/java/com/robert/ecommerce/rest/LoggerRestController.java
+++ ecommerce-rest/src/main/java/com/robert/ecommerce/rest/LoggerRestController.java
@@ -1,6 +1,8 @@
 package com.robert.ecommerce.rest;
 
 import com.robert.ecommerce.dto.LoggingDTO;
+import com.robert.ecommerce.entity.LaptopRecordEntity;
+import com.robert.ecommerce.repository.LoggerRepository;
 import com.robert.ecommerce.service.LoggerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class LoggerRestController {
@@ -16,9 +19,17 @@ public class LoggerRestController {
     @Autowired
     private LoggerService loggerService;
 
+    @Autowired
+    private LoggerRepository loggerRepository;
+
     @RequestMapping(method = POST, path = "api/all/log", produces = APPLICATION_JSON_VALUE)
     public void registerNewLog(@RequestBody LoggingDTO log) {
         System.out.println(log);
         loggerService.registerNewLog(log);
+    }
+
+    @RequestMapping(method = GET, path = "api/all/log", produces = APPLICATION_JSON_VALUE)
+    public void getBlabla() {
+        loggerRepository.getRecords().forEach(item -> System.out.println(item.toString()));
     }
 }
